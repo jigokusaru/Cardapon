331b23a04f819cf907ad5dcf9a7ea38ece9a2d55
Abbey Joubert <joubertjr.abbey@gmail.com>
2026-01-21 19:44:19 -0500

    Stuff

--- src/main/java/com/cardapon/core/data/MoveCategory.java
+++
@@ -1,7 +0,0 @@
-package com.cardapon.core.data;
-
-public enum MoveCategory {
-    PHYSICAL, // Uses ATK vs DEF
-    AURA,     // Uses AUR vs RES
-    STATUS    // Non-damaging (Buffs, Debuffs, Hazards)
-}
