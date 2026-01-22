572e82672cc4b0216958fbf41c48078412be50f4
Abbey Joubert <joubertjr.abbey@gmail.com>
2026-01-21 19:46:59 -0500

    Stuff

--- src/main/java/com/jigokusaru/cardapon/CardaponPlugin.java
+++ src/main/java/com/jigokusaru/cardapon/CardaponPlugin.java
@@ -1,19 +1,19 @@
-package com.example.exampleplugin;
+package com.jigokusaru.cardapon;
 
 import com.hypixel.hytale.logger.HytaleLogger;
 import com.hypixel.hytale.server.core.plugin.JavaPlugin;
 import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
 
-public class ExamplePlugin extends JavaPlugin {
+public class CardaponPlugin extends JavaPlugin {
     private static final HytaleLogger LOGGER = HytaleLogger.forEnclosingClass();
 
-    public ExamplePlugin(JavaPluginInit init) {
+    public CardaponPlugin(JavaPluginInit init) {
         super(init);
         LOGGER.atInfo().log("Hello from %s version %s", this.getName(), this.getManifest().getVersion().toString());
     }
 
     @Override
     protected void setup() {
-        this.getCommandRegistry().registerCommand(new ExampleCommand(this.getName(), this.getManifest().getVersion().toString()));
+        // Register commands and listeners here
     }
 }
